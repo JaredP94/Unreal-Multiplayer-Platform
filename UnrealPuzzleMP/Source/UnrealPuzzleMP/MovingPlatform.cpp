1cba10eb175fa6fce9dd087cf146d417d31c7d45
Jared <jaredping@yahoo.com>
2019-03-21 00:05:11 +0200

    Create elevator functionality for platform

--- UnrealPuzzleMP/Source/UnrealPuzzleMP/MovingPlatform.cpp
+++ UnrealPuzzleMP/Source/UnrealPuzzleMP/MovingPlatform.cpp
@@ -18,6 +18,9 @@ void AMovingPlatform::BeginPlay()
 		SetReplicates(true);
 		SetReplicateMovement(true);
 	}
+
+	GlobalStartLocation = GetActorLocation();
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
 }
 
 void AMovingPlatform::Tick(float DeltaTime)
@@ -27,8 +30,17 @@ void AMovingPlatform::Tick(float DeltaTime)
 	if (HasAuthority())
 	{
 		FVector Location = GetActorLocation();
-		FVector GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
-		FVector Direction = (GlobalTargetLocation - Location).GetSafeNormal();
+		float JournyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+		float JourneyTravelled = (Location - GlobalStartLocation).Size();
+
+		if (JourneyTravelled >= JournyLength)
+		{
+			FVector Temp = GlobalStartLocation;
+			GlobalStartLocation = GlobalTargetLocation;
+			GlobalTargetLocation = Temp;
+		}
+
+		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
 		Location += (Speed * DeltaTime * Direction);
 		SetActorLocation(Location);
 	}
