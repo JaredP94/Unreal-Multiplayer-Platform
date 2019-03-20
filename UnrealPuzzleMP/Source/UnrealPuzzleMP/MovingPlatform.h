61b6574dfe4920e380bab0064f0d0df5deb31e97
Jared <jaredping@yahoo.com>
2019-03-20 22:15:35 +0200

    Test client vs server events

---
+++ UnrealPuzzleMP/Source/UnrealPuzzleMP/MovingPlatform.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPUZZLEMP_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMovingPlatform();
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float Speed = 20;
+};
