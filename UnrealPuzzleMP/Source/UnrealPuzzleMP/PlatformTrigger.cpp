acd06ded66a31e8c1ca45a85e3efd8220d3377fa
Jared <jaredping@yahoo.com>
2019-03-22 21:54:00 +0200

    Overlapping events for pressure pads

--- UnrealPuzzleMP/Source/UnrealPuzzleMP/PlatformTrigger.cpp
+++ UnrealPuzzleMP/Source/UnrealPuzzleMP/PlatformTrigger.cpp
@@ -21,7 +21,12 @@ APlatformTrigger::APlatformTrigger()
 void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (!TriggerVolume)
+		return;
+
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called every frame
@@ -31,3 +36,12 @@ void APlatformTrigger::Tick(float DeltaTime)
 
 }
 
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated pad"));
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated pad"));
+}
--- UnrealPuzzleMP/Source/UnrealPuzzleMP/PlatformTrigger.h
+++ UnrealPuzzleMP/Source/UnrealPuzzleMP/PlatformTrigger.h
@@ -24,6 +24,12 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 private:
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex);
+	
 	UPROPERTY(VisibleAnywhere)
 	class UBoxComponent* TriggerVolume;
 };
